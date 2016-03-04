5c9c9fda332d13c5f00a633e1add15a27b0345ef
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-03-04 14:13:59 +0200

    bug fix

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ def save_to_mongo(paper_id):
     colletion.insert_one(paper)
 
 
-def main(paper_colletion_numbers, number_paper_in_colletion):
-    for i in paper_colletion_numbers:
-        for j in range(number_paper_in_colletion):
-            save_to_mongo('10.1.1.%d.%d' % (i, j))
+def main(paper_colletion_number, number_paper_in_colletion):
+    for j in range(1, number_paper_in_colletion):
+        save_to_mongo('10.1.1.%d.%d' % (paper_colletion_number, j))
 
 
 if __name__ == '__main__':
-    args = str(sys.argv)
-    main(args[1], args[2])
+    main(int(sys.argv[1]), int(sys.argv[2]))
